183b0fd748a783aeaf4ba3ff06679f4993011996
lubi888 <linuxubiquitous@gmail.com>
2016-03-12 09:49:26 +0000

    clean  Changes to be committed: 	modified:   lib/dart_cc.dart 	modified:   lib/dart_cc.html 	modified:   web/index.dart 	modified:   web/index.html 	modified:   web/styles.css

--- web/index.dart
+++ web/index.dart
@@ -1,5 +1,3 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
 library my_project.web.index;
 
 import 'package:dart_polymer_cc_appeng/dart_cc.dart';
